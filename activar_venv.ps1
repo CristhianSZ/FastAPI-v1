784f8baddbfa4391e520a1631ab8b71d7c6106c7
CristhianSZ <cristhiansosazurita@gmail.com>
2025-09-23 13:40:00 -0300

    se agrega el esquema de usuario y asocia con la creaicon de de token, ademas se agrega la funcion de validacion de token

---
+++ activar_venv.ps1
@@ -0,0 +1,3 @@
+Write-Host "Activando el entorno virtual para PowerShell..."
+. .\venv\Scripts\Activate.ps1
+Write-Host "Entorno virtual activado. Usa 'deactivate' para salir."
--- jwt.py
+++
@@ -1,6 +0,0 @@
-import jwt
-
-def createToken(data: dict):
-    token : str = jwt.encode(payload=data, key="secret", algorithm="HS256")
-    return token
-
