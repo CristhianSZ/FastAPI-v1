d5da1951390859d02dd87c87b7532806f020c4b0
CristhianSZ <cristhiansosazurita@gmail.com>
2025-09-24 20:32:58 -0300

    se agrega la conexion con la base de datos

--- activar_venv.ps1
+++ activar_venv.ps1
@@ -1,3 +1,4 @@
 Write-Host "Activando el entorno virtual para PowerShell..."
 . .\venv\Scripts\Activate.ps1
 Write-Host "Entorno virtual activado. Usa 'deactivate' para salir."
+Write-Host "Entorno virtual activado. Usa 'uvicorn main:app --reload --port 4000' iniciar el servidor."
